0cfc4449f5a63fdea3dd8b9427bb0e09a7b3d9b8
fadiabji <fadi.abji@htomail.com>
2023-02-27 12:19:47 +0100

    data base has created successfully

--- Movie_Exercise/Models/Order.cs
+++ Movie_Exercise/Models/Order.cs
@@ -4,6 +4,8 @@ namespace Movie_Exercise.Models
 {
     public class Order
     {
+        [Key]
+        public int Id { get; set; }
         [Required]
         [Display(Name = "Order Date")]
         public DateTime OrderDate { get; set; } = DateTime.Now;
