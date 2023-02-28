6bbbf11bc2fdb207b48cb974aabb7cf10ef2adaa
fadiabji <fadi.abji@htomail.com>
2023-02-28 10:34:13 +0100

    adding services to Move model

---
+++ Movie_Exercise/Services/IMovieService.cs
@@ -0,0 +1,7 @@
+﻿namespace Movie_Exercise.Services
+{
+    public interface IMovieService
+    {
+
+    }
+}
---
+++ Movie_Exercise/Services/MovieService.cs
@@ -0,0 +1,15 @@
+﻿using Movie_Exercise.Data;
+
+namespace Movie_Exercise.Services
+{
+    public class MovieService : IMovieService
+    {
+        private readonly ApplicationDbContext _db;
+        public MovieService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        
+    }
+}
