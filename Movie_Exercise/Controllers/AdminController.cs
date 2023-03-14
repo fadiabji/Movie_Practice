06f7232fe0825dc70702bd0e6eb117c59f59d0e7
fadiabji <fadi.abji@htomail.com>
2023-03-14 13:34:28 +0100

    adding new layout and admin controller

---
+++ Movie_Exercise/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Movie_Exercise.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
