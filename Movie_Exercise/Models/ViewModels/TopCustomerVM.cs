4c951d6a381a17e4e26637ed6cf93a44db08537d
fadiabji <fadi.abji@htomail.com>
2023-04-09 14:00:45 +0200

    adding view modles for all things on landing page

---
+++ Movie_Exercise/Models/ViewModels/TopCustomerVM.cs
@@ -0,0 +1,8 @@
+﻿namespace Movie_Exercise.Models.ViewModels
+{
+    public class TopCustomerVM
+    {
+        public Customer customer { get; set; }
+        public Order order { get; set; }
+    }
+}
---
+++ Movie_Exercise/ViewComponents/CheapestMovies.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movie_Exercise.Data;
+using Movie_Exercise.Models;
+
+namespace Movie_Exercise.ViewComponents
+{
+    public class CheapestMovies : ViewComponent
+    {
+        private readonly ApplicationDbContext _db;
+        public CheapestMovies(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IViewComponentResult Invoke()
+        {
+            //    // The 5 Cheapest movies based on price
+                var cheapestMovies = _db.Movies.Select(m => m).OrderBy(m => m.Price).ThenBy(m => m.Title).Take(5).ToList();
+            return View("Index", cheapestMovies);
+        }
+
+    }
+}
